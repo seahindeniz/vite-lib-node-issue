157406488d4f3e28057ae05ec2a416fdac29bb04
Sahin Deniz <seahindeniz@gmail.com>
2023-07-23 00:59:04 +0300

    Specify target

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,17 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  esbuild: {
+    platform: 'node',
+    target: 'node16',
+  },
   build: {
+    target: 'node16',
     lib: {
       entry: resolve(__dirname, 'src/main.ts'),
       fileName: 'main',
       name: 'main',
-      formats: ['es', 'cjs', 'umd', 'iife'],
+      formats: ['es', 'cjs'],
     },
   },
 })
